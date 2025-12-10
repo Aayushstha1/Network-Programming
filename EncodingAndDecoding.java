dd3bcc52eeb192402c6b9c077135167562e7a6b3
Aayushstha1 <sudipk339@gmail.com>
2025-12-10 08:38:19 +0545

    Encoding

--- EncodingAndDecoding.java
+++ EncodingAndDecoding.java
@@ -1,3 +1,11 @@
+import java.net.URLEncoder;
+
 public class EncodingAndDecoding {
+    public static void main(String[] args) throws Exception {
+        String text = "Hello@Aayush";
+        System.out.println(URLEncoder.encode(text, "UTF-8"));
+        String Hari = "Hello#_hari";
+        System.out.println(URLEncoder.encode(Hari, "utf-8"));
+    }
     
 }
