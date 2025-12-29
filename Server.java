0b1324a278b40bc914ba8f9751113318e9d753b0
Aayushstha1 <sudipk339@gmail.com>
2025-12-29 10:11:19 +0545

    new

--- Server.java
+++ Server.java
@@ -1,28 +1,30 @@
 import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(5000);
-            System.out.println("Server is running...");
-
-            Socket socket = serverSocket.accept(); 
-            System.out.println("Client connected: " );
-
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-
-            writer.println("Hello from the server!");
-
+public class Server{
+    public static void main(String[] args){
+        try{
+            ServerSocket Ssocket=new ServerSocket(5000);
+            System.out.println("Server is running... waiting for client connection");
+            Socket socket =Ssocket.accept();
+            System.out.println("Clinet is connected");
+            OutputStream output=socket.getOutputStream();
+            PrintWriter writer=new PrintWriter(output,true);
+            writer.println("Hello Hemnarayan");
+            BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String clientMessage=reader.readLine();
+            System.out.println("Message from client: "+clientMessage);
             writer.close();
             socket.close();
-            serverSocket.close();
-
-        } catch (Exception ex) {
+            Ssocket.close();
+        }
+        catch(Exception ex){
             ex.printStackTrace();
         }
+       
     }
 }
